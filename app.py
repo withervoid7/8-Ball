d34063f46924efbc3f6c3b834236ee0eb15886d2
withervoid7 <withervoid7@gmail.com>
2025-09-12 05:51:09 +0000

    uštimu

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import random
+# pip install flask
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET"])
+def index():
+
+    return render_template("index.html")
+    #return "<p>Hellooooooooo World!</p>"
+
+
+@app.route("/8-Ball", methods = ["POST"])
+def Ball():
+    r=random.choice("yes","maybe","no")
+    if r == "yes":
+        return random.choice("As I see it, yes","Most likely","Outlook good","Yes")
+    elif r == "maybe":
+        return random.choice("Reply hazy, try again","Ask again later","Better not tell you now","Cannot predict now")
+    else:
+        return random.choice("Don't count on it","My reply is no","Outlook not so good","Very doubtful")
+
+app.run(debug=True)
+
+"""    tmp = dict(request.form)
+    ime1 = tmp.get("ime1")
+    ime2 = tmp.get("ime2")
+    r = f"{ime1} + {ime2} = {random.randint(0,100)} %"
+    return render_template("index.html", rezultat = r)
+    #return f"{ime1} + {ime2} = {random.randint(0,100)} %" """
+
